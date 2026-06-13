30f030ee80cab725a49b806d9225851d5fa227a3
Michael Crismali <michael@crismali.com>
2026-06-13 16:45:06 -0500

    Added footer

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
 console.log('Site loaded')
+
+function setCurrentYear() {
+  const yearEl = document.getElementById('year')
+  if (yearEl) yearEl.textContent = new Date().getFullYear().toString()
+}
+
+setCurrentYear()
