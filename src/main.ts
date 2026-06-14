8585b59285660c735212777d842528a3a544cf0d
Michael Crismali <michael@crismali.com>
2026-06-14 12:17:54 -0500

    Added a contact link

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,13 @@ function setCurrentYear() {
   if (yearEl) yearEl.textContent = new Date().getFullYear().toString()
 }
 
+function setContactLink() {
+  const el = document.getElementById('contact-link')
+  if (!el) return
+  const parts = ['michael', '@', 'crismali', '.', 'com']
+  const email = parts.join('')
+  el.setAttribute('href', `mailto:${email}`)
+}
+
 setCurrentYear()
+setContactLink()
